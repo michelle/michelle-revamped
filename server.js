f86e79c8215bd341dad7a5003178db3609376ae4
Michelle Bu <analogmidnight@gmail.com>
2012-12-18 22:35:49 -0800

    favicon

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app =  express.createServer();
 app.use(express.bodyParser());
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.favicon(__dirname + '/public/favicon.png'));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
